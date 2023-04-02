96283e76777120e9d4b0e1848ce383651e7fc2af
enuar rosales <enuaremiliorosales@gmail.com>
2023-04-01 23:07:32 -0500

    se crea proyecto

---
+++ src/test/java/com/example/SistemaAdministracion/SistemaAdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaAdministracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAdministracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
